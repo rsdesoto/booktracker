ba7efa9c48c50c810fe94b77bcb1a9a6fea1c0ef
Theo <ry.e.desoto@gmail.com>
2021-01-17 13:49:56 -0600

    Testing updates

--- client/src/components/Test2.jsx
+++ client/src/components/Test2.jsx
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 function Example() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
-  
+  const [books, setBooks] = useState([]);
+
+  console.log(count)
+
   return (
     <div>
       <p>You clicked {count} times</p>
